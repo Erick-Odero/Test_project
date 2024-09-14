f0cf26bcbbb28c7269daef01b063a0a32c3b8ffc
unknown <oderoerique@gmail.com>
2024-09-14 18:21:29 +0300

    Creating matplotlib

--- Python_matplotlib.py
+++ Python_matplotlib.py
@@ -1,9 +1,8 @@
-#Most of the Matplotlib utilities lies under the pyplot submodule, and are usually imported under the plt alias:
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt  # Import the matplotlib.pyplot library for creating visualizations/plots.
+import numpy as np  # Import the numpy library for working with arrays and numerical computations.
 
-xpoints = np.array([0, 9])
-ypoints = np.array([0, 2500])
+xpoints = np.array([0, 9])  # Create a numpy array for the x-axis points: 0 and 9.
+ypoints = np.array([0, 2500])  # Create a numpy array for the y-axis points: 0 and 2500.
 
-plt.plot(xpoints, ypoints)
-plt.show()
+plt.plot(xpoints, ypoints)  # Plot the x and y points as a line on a graph.
+plt.show()  # Display the plot in a window.
