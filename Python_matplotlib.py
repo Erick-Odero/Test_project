39479c1dd03d3449dcece7b2c5916448b8e6cfd6
unknown <oderoerique@gmail.com>
2024-09-14 18:18:18 +0300

    Python matplotlib

--- Python_matplotlib.py
+++ Python_matplotlib.py
@@ -1,8 +1,9 @@
+#Most of the Matplotlib utilities lies under the pyplot submodule, and are usually imported under the plt alias:
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+xpoints = np.array([0, 9])
+ypoints = np.array([0, 2500])
 
 plt.plot(xpoints, ypoints)
 plt.show()
